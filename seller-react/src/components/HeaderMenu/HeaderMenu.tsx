1e88f51d58e29a5df8cb2972dab318f4d3d53ce1
AlexPal005 <rudenkoolexandr06@knu.ua>
2024-02-04 23:47:47 +0200

    added drop down profile menu

--- seller-react/src/components/HeaderMenu/HeaderMenu.tsx
+++ seller-react/src/components/HeaderMenu/HeaderMenu.tsx
@@ -5,28 +5,50 @@ import {FaRegHeart} from "react-icons/fa";
 import {RiAccountCircleLine} from "react-icons/ri";
 import {IoIosArrowDown} from "react-icons/io";
 import {useNavigate} from "react-router-dom";
+import {ProfileMenu} from "../ProfileMenu/ProfileMenu.tsx";
+import {useState} from "react";
 
 export const HeaderMenu = () => {
     const navigate = useNavigate();
+    const [isHoverProfile, setIsHoverProfile] = useState(false)
     const redirectCreatePost = (): void => {
         navigate('/create-post');
     }
+    const showProfileMenu = () => {
+        setIsHoverProfile(true)
+    }
+    const hideProfileMenu = () => {
+        setIsHoverProfile(false)
+    }
+
     return (
         <div className='main-menu'>
             <ul className='main-menu__list'>
-                <li className='main-menu__item'>
-                    <FiMessageSquare className='main-menu__icon  main-menu__icon_margin_r'/>
+                <li className='main-menu__item main-menu__item_color'>
+                    <FiMessageSquare className='main-menu__icon  main-menu__icon_margin-r'/>
                     <span>Повідомлення</span>
                 </li>
-                <li className='main-menu__item'>
+                <li className='main-menu__item main-menu__item_color'>
                     <FaRegHeart className='main-menu__icon'/>
                 </li>
-                <li className='main-menu__item'>
-                    <RiAccountCircleLine className='main-menu__icon main-menu__icon_margin_r'/>
-                    <span>Мій профіль</span>
-                    <IoIosArrowDown className='main-menu__icon main-menu__icon-arrow'/>
+                <li className='main-menu__item '
+                    onMouseOver={showProfileMenu}
+                >
+                    <div className='main-menu__profile main-menu__item_color'>
+                        <RiAccountCircleLine className='main-menu__icon main-menu__icon_margin-r'/>
+                        <span>Мій профіль</span>
+                        <IoIosArrowDown className='main-menu__icon main-menu__icon-arrow'/>
+                    </div>
+
+                    {
+                        isHoverProfile &&
+                        <ProfileMenu
+                            showProfileMenu={showProfileMenu}
+                            hideProfileMenu={hideProfileMenu}
+                        />
+                    }
                 </li>
-                <li className='main-menu__item'>
+                <li className='main-menu__item main-menu__item-hover'>
                     <Button text={'Додати оголошення'} onClick={redirectCreatePost} className='main-menu__button'/>
                 </li>
             </ul>
---
+++ seller-react/src/components/ProfileMenu/ProfileMenu.tsx
@@ -0,0 +1,50 @@
+import './profile-menu.scss'
+import {Link} from "react-router-dom";
+
+interface profileMenuProps {
+    showProfileMenu: () => void;
+    hideProfileMenu: () => void;
+
+}
+
+export const ProfileMenu = ({showProfileMenu, hideProfileMenu}: profileMenuProps) => {
+    return (
+        <div className='profile-menu profile-menu_position'
+             onMouseOver={showProfileMenu}
+             onMouseOut={hideProfileMenu}
+        >
+            <div className='profile-menu__user-block'>
+                <img src='src/assets/detskiy-mir-36-1x.png' alt={'user'} className='profile-menu__user-photo'/>
+                <p className='profile-menu__user-name'>Олександр</p>
+            </div>
+            <p className='profile-menu__category-item'>Ваш профіль</p>
+            <ul className='profile-menu__list'>
+                <li className='profile-menu__item'><Link to='/' className='profile-menu__item-link'>Оголошення</Link>
+                </li>
+                <li className='profile-menu__item'><Link to='/' className='profile-menu__item-link'>Повідомлення</Link>
+                </li>
+                <li className='profile-menu__item'><Link to='/' className='profile-menu__item-link'>Налаштування</Link>
+                </li>
+                <li className='profile-menu__item'><Link to='/' className='profile-menu__item-link'>Платежі та
+                    рахунок</Link></li>
+            </ul>
+            <p className='profile-menu__category-item'>Обрані</p>
+            <ul className='profile-menu__list'>
+                <li className='profile-menu__item profile-menu__item_position'>
+                    <Link to='/' className='profile-menu__item-link'>Оголошення</Link>
+                    <div className='counter'>
+                        <span className='counter__text'>1</span>
+                    </div>
+                </li>
+                <li className='profile-menu__item profile-menu__item_position'>
+                    <Link to='/' className='profile-menu__item-link'>Пошуки</Link>
+                    <div className='counter'>
+                        <span className='counter__text'>0</span>
+                    </div>
+                </li>
+            </ul>
+            <hr/>
+            <a className='profile-menu__close-button' href='/'>Вийти</a>
+        </div>
+    )
+}
