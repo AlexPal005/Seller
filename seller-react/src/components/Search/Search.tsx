619a1b4bfcb8ac5ab27b77d02df1dfee38cc31d2
AlexPal005 <rudenkoolexandr06@knu.ua>
2024-02-07 00:14:13 +0200

    added recent search table

--- seller-react/src/components/Search/Search.tsx
+++ seller-react/src/components/Search/Search.tsx
@@ -2,13 +2,32 @@ import './search.scss'
 import {Button} from "../Button/Button.tsx";
 import {IoSearchOutline} from "react-icons/io5";
 import {FaMapMarkerAlt} from "react-icons/fa";
+import {SearchTable} from "../SearchTable/SearchTable.tsx";
+import {useState} from "react";
 
 export const Search = () => {
+    const [isClickedSearch, setIsClickedSearch] = useState(false)
+
+    const showSearchTable = () => {
+        setIsClickedSearch(true)
+    }
+    const hideSearchTable = () => {
+        setIsClickedSearch(false)
+    }
     return (
         <div className='search'>
             <div className="search__block">
                 <IoSearchOutline className='search__icon'/>
-                <input type='text' className='search__text' placeholder='Що шукаєте?'/>
+                <input type='text'
+                       className='search__text'
+                       placeholder='Що шукаєте?'
+                       onFocus={showSearchTable}
+                       onBlur={hideSearchTable}
+                />
+                {
+                    isClickedSearch &&
+                    <SearchTable/>
+                }
             </div>
             <div className='search__block'>
                 <input type='text' className='search__region' placeholder='Уся Україна'/>
---
+++ seller-react/src/components/SearchTable/SearchTable.tsx
@@ -0,0 +1,21 @@
+import './search-table.scss'
+
+export const SearchTable = () => {
+    return (
+        <div className='search-table'>
+            <p className='search-table__title'>Нещодавний пошук</p>
+            <hr/>
+            <SerchItem/>
+            <SerchItem/>
+        </div>
+    )
+}
+
+const SerchItem = () => {
+    return (
+        <div className='search-item'>
+            <p className='search-item__text'>Автомобільна фарба</p>
+            <span className='search-item__filters-text'>Пошукові фільтри [<span>4</span>]</span>
+        </div>
+    )
+}
