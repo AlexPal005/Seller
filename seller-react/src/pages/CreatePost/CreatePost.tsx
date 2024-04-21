0b9ea042c3473f08a3e6c0a2a943a2c521609f86
AlexPal005 <rudenkoolexandr06@knu.ua>
2024-04-21 14:41:28 +0300

    added create post details

--- seller-react/src/pages/CreatePost/CreatePost.tsx
+++ seller-react/src/pages/CreatePost/CreatePost.tsx
@@ -1,7 +1,11 @@
 import './create-post.scss'
+import {DetailsCreatePost} from "./DetailsCreatePost.tsx";
 
 export const CreatePost = () => {
     return (
-        <>create</>
+        <div className='create-post-container'>
+            <h2 className='create-post-container__title'>Створити оголошення</h2>
+            <DetailsCreatePost/>
+        </div>
     )
 }
