6d12b6ad2ae872324e0ac5726e203617bb6e7e9e
AlexPal005 <rudenkoolexandr06@knu.ua>
2024-06-16 23:03:29 +0300

    preloader

---
+++ seller-react/src/components/Preloader/Preloader.tsx
@@ -0,0 +1,19 @@
+import './preloader.scss'
+
+export const Preloader = () => {
+    return (
+        <div className="preloader">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+
+    )
+}
